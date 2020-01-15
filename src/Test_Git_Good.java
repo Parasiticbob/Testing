2f49ac4a852e9e4cb5754f3ccd5bd6322e9999b9
unknown <erod2525@yahoo.com>
2020-01-15 08:05:23 -0500

    First commit

---
+++ src/Test_Git_Good.java
@@ -0,0 +1,10 @@
+
+public class Test_Git_Good {
+
+	public static void main(String[] args) {
+		System.out.printf("Hello World%n");
+		System.out.printf("This is my second try to make Github work%n");
+		System.out.printf("Wish me luck!!!");
+	}
+
+}
